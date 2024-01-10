b7d89dbfa6a264f1a3026b77c21f779576fa3ac3
Sebasss <sebas.cruz750@gmail.com>
2024-01-09 23:31:55 -0500

    title changed

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Login/> {/*Esta parte servirá para cambiar de componetenes Login, Lista, Add*/}
+      <Add/> {/*Esta parte servirá para cambiar de componetenes Login, Lista, Add*/}
     </ThemeProvider>
   </React.StrictMode>
 );
